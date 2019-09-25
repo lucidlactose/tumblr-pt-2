e14f2507802a78a0186b9148fec24d589e0d3334
lucidlactose <jesusvillarruel98@gmail.com>
2019-09-25 16:46:49 -0700

    touching it does something

--- storyboard/storyboard/PhotoViewController.swift
+++ storyboard/storyboard/PhotoViewController.swift
@@ -71,15 +71,15 @@ class PhotoViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         return cell
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let post = posts[indexPath.row]
+        let photoViewController = segue.destination as! PhotoDetailsViewController
+
+        photoViewController.post = post
     }
-    */
 
 }
